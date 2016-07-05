52a7987a09df404d77067b40b76f13a79a21d8da
Odomontois <odomontois@gmail.com>
2016-07-05 18:43:49 +0300

    some board features

--- src/game.js
+++ src/game.js
@@ -2,7 +2,7 @@ import Board from "./states/Board"
 
 export default class Game extends Phaser.Game {
   constructor(){
-      super(800, 1200, Phaser.AUTO, '', null)
+      super(800, 600, Phaser.AUTO, '', null)
       this.state.add('Board', Board, false)
       this.state.start('Board')
   }
--- src/states/Board.js
+++ src/states/Board.js
@@ -1,11 +1,31 @@
 export default class Board extends Phaser.State{
   preload () {
-    this.game.load.image('selfie', 'photos/selfie.jpg')
+    const {game} = this
+    game.load.image('selfie', 'photos/selfie.jpg')
+    game.load.image('bg', "images/background.jpg")
+    game.load.json('stages', 'data/stages.json')
+    const stages = game.cache.getJSON('stages')
+    for ([name, {photo}] of Object.entries(stages)){
+      game.load.image(`stage_{name}`, `photos/{photo}`)
+    }
+  }
+
+  drawStage({x,y,photo,crop}){
+    const pic = this.game.add.sprite(x,y, `stage_{name}`)
   }
 
   create() {
       const {game} = this
-      const logo = game.add.sprite(game.world.centerX, game.world.centerY, 'selfie')
-      logo.anchor.setTo(0.5, 0.5)
+      game.add.sprite(0,0,"bg")
+
+      const stages = game.cache.getJSON('stages');
+      console.log(stages);
+
+      for(const [name, stage] of Object.entries(stages)){
+        console.log(name, stage)
+      }
+  }
+
+  render(){
   }
 }
